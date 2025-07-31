6dca16a73012fafc02cf4ef8f51379233d0f4757
AayushSahani01 <anshusahani6104@gmail.com>
2025-07-31 10:40:26 +0530

    add to Topbar.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import Header from "./components/Header";
 import Home from "./pages/Home";
 import Footer from "./components/Footer";
+import OnTopBar from "./components/TopBar";
 
 function App() {
 	return (
@@ -8,6 +9,7 @@ function App() {
 			<Header />
 			<Home />
 			<Footer />
+			<OnTopBar />
 		</div>
 	);
 }
